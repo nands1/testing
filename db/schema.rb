05abc4687da2838d44671a362643d8ae6afcea14
Nandini Nayak <nandininayak@nandinis-mbp.ait-sydney.net.au>
2018-02-01 10:26:18 +1100

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160630072026) do
+ActiveRecord::Schema.define(version: 20180131232129) do
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "age"
+    t.text     "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
